a67a291a1ea673a0ead98300f93593e18be43086
Yorki <36930392+Daiyouqi@users.noreply.github.com>
2024-07-24 21:15:31 +0800

    Update Convenience_QThread.h

--- Convenience_QThread.h
+++ Convenience_QThread.h
@@ -4,33 +4,33 @@
 #include <shared_mutex>
 #include <QThread>
 #include <set>
-class Convenience_QThread : public QThread
+class ConvenienceQThread : public QThread
 {
 	Q_OBJECT
 public:
 	/*外部接口1:添加任务接口*/
-	int async_task(std::function<void()>task1, std::function<void()> task2);
+	int asyncTask(std::function<void()>task, std::function<void()> callback);
 	//主接口,方便的将同步代码转成异步代码.第一个调用对象为qthread执行,第二个这个类所在线程执行的回调
-	int  sync_task(std::function<void()>task1, std::function<void()> task2);//同步测试代码
+	int syncTask(std::function<void()>task, std::function<void()> callback);//同步测试代码
 
 	/*外部接口2:任务管理接口,如你不满意可自行添加*/
-	void cancel_task(int uuid);//删除指定任务,线程安全
-	void cancel_all_task();//删除所有任务,线程安全
-	void add_important_mark(int uuid);
+	void cancelTask(int uuid);//删除指定任务,线程安全
+	void cancelAllTask();//删除所有任务,线程安全
+	void addImportantMark(int uuid);
 	//使一个任务为重要任务,无法被cancel_all_order撤单
 	//比如网络通信中用户可能有"清除所有的发送数据task,但保留接受数据task"的需求
 
 	/*外部接口3,删除接口,异步*/
-	void Async_delete();//请连接 delete_finish信号
+	void asyncDelete();//请连接 delete_finish信号
 	//这个类是个QThread子类,在你删除它之前,
 	//请先调用这个函数将将其disable掉,这个操作是异步的,异步删除思路为主动取消所有待执行任务,再断开回调执行对象.
-        //最后查询当前任务自增ID,添加一个空任务,当这个空任务执行完成后,发送delete_finish信号. 然后,可以按对未执行的QThread的方式删除对象.
+		//最后查询当前任务自增ID,添加一个空任务,当这个空任务执行完成后,发送delete_finish信号. 然后,可以按对未执行的QThread的方式删除对象.
 
 	/*外部接口4,用户的异步函数和回调函数调用,检查当前任务是否应该被提前终止*/
-	bool check_eligible();
+	bool checkEligible();
 
-	Convenience_QThread(QThread* parent = nullptr);
-	~Convenience_QThread();/*对象可直接delete不崩溃,但不推荐,推荐用Async_delete*/
+	ConvenienceQThread(QThread* parent = nullptr);
+	~ConvenienceQThread();/*对象可直接delete不崩溃,但不推荐,推荐用asyncDelete*/
 private:
 
 	const int THREAD_IDLE = -1;
@@ -49,10 +49,10 @@ private:
 
 	Qt::HANDLE m_child_ThreadID;
 	bool m_is_async_delete = false;
-	int m_delete_task_uuid;
+	int m_delete_task_uuid = 0;
 protected:
 	void run();
 signals:
-	void please_execute_callback(int uuid);//请勿使用，这个信号是类内部使用的
-	void delete_finish();//当调用Async_delete异步删除接口后，准备删除工作完成时会发送这个信号
+	void pleaseExecuteCallback(int uuid);//请勿使用，这个信号是类内部使用的
+	void deleteFinish();//当调用Async_delete异步删除接口后，准备删除工作完成时会发送这个信号
 };
